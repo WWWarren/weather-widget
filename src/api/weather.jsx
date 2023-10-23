f8b9c3968b26b74f5ef32fdb0b6e96bbd8a67d2d
WWWarren <49315386+WWWarren@users.noreply.github.com>
2023-10-23 11:50:18 +0100

    added error message if API not wokring

--- src/api/weather.jsx
+++ src/api/weather.jsx
@@ -1,9 +1,5 @@
 export async function getWeatherData(params) {
     let response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${params.lat}&lon=${params.lon}&units=${params.units ? params.units : 'metric'}&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`)
     response = await response.json()
-    if (response) {
-        return response
-    } else {
-        return 'Could not return data'
-    }
+    return response
 }
--- src/components/dynamic/WeatherWidget.jsx
+++ src/components/dynamic/WeatherWidget.jsx
@@ -21,7 +21,17 @@ const WeatherWidget = ({ city }) => {
         return () => {};
     }, [city, units])
 
-    if (city && weather && weather.city) {
+    // Handle error returned by API
+    if (weather && weather.cod === (401 || 400 || 500)) {
+        return (
+            <div className="mt-6 max-w-screen-md w-full">
+                <div className="bg-white rounded text-black shadow-lg animate-fade-down animate-once animate-ease-in-out p-12 h-72">
+                    <h1 className="text-4xl font-bold mb-1">{weather.cod}: Weather data not found</h1>
+                    <h2 className="text-2xl font-semibold mb-4">Please try again later</h2>
+                </div>
+            </div>
+        )
+    } else if (city && weather && weather.city) {
         //slice weather array to get time segments for sub cards
         const forecastWeather = weather.list.slice(1, 6);
 
